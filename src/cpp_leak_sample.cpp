e65ca41e3de69dde9608d72d146d0ffdde61ef45
Abel_PC <569879601@qq.com>
2021-03-31 08:21:56 +0800

    提交一份测试代码；

--- src/cpp_leak_sample.cpp
+++ src/cpp_leak_sample.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <random>
-#include <vld.h>
+#include <vector>
 
-int main(int agrn, char* argc[]) {
+// clang++ -o main -fsanitize=address -std=c++11 ./main.cpp
+
+void leak_sample1()
+{
 	std::uniform_int_distribution<unsigned> u(0, 9);
 	std::default_random_engine e;
 	char* ptr = nullptr;
-	for (int i=0;i<10;++i)
+	for (int i = 0; i < 10; ++i)
 	{
 		auto n = u(e);
 		if (n < 5)
@@ -29,5 +32,34 @@ int main(int agrn, char* argc[]) {
 	{
 		delete[]ptr;
 	}
+}
+
+void leak_sample2()
+{
+	std::uniform_int_distribution<unsigned> u(0, 9);
+	std::default_random_engine e;
+	char* ptr = nullptr;
+	std::vector<char*> vec;
+	for (int i = 0; i < 10; ++i)
+	{
+		auto n = u(e);
+		if (n < 5)
+		{
+			ptr = new char[n];
+			vec.push_back(ptr);
+		}
+		else
+		{
+			ptr = new char[n];
+			vec.push_back(ptr);
+			delete ptr;
+		}
+	}
+	vec.clear();
+}
+
+int main(int agrn, char* argc[])
+{
+	leak_sample1();
 	return 0;
 }
