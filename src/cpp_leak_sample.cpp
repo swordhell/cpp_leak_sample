f833ac0ec17a911ece73dbb1ce5c4cfca9519273
Abel_PC <569879601@qq.com>
2021-10-22 17:59:55 +0800

    测试内存泄漏；

--- src/cpp_leak_sample.cpp
+++ src/cpp_leak_sample.cpp
@@ -3,8 +3,12 @@
 #include <vector>
 #include <string.h>
 
-// clang++ -o main -D_GLIBCXX_DEBUG -fsanitize=address -std=c++11 -O1 -g ./cpp_leak_sample.cpp
+#include <chrono>
+#include <thread>
 
+// clang++ -o main -D_GLIBCXX_DEBUG -fsanitize=address -std=c++11 -O1 -g ./cpp_leak_sample.cpp
+// cmake .. -DCMAKE_CXX_COMPILER=clang++
+// g++ -o main -std=c++11 -O1 -g main.cpp
 void leak_sample1()
 {
 	std::uniform_int_distribution<unsigned> u(0, 9);
@@ -62,9 +66,22 @@ void leak_sample2()
 	vec.clear();
 }
 
+void test_new_delete()
+{
+	char *buff = nullptr;
+	for (;;)
+	{
+		buff = new char[1024];
+		delete buff;
+		std::this_thread::sleep_for(std::chrono::seconds(10));
+
+	}
+}
+
 int main(int agrn, char* argc[])
 {
-	leak_sample1();
-	leak_sample2();
+	//leak_sample1();
+	//leak_sample2();
+	test_new_delete();
 	return 0;
 }
